486d9c1fb9c0267a6fa24f0d0ce7ca690925b255
wafel <thewafelek@gmail.com>
2023-05-08 22:00:48 +0200

    Added simple parsing logic for dataset

--- utils.py
+++ utils.py
@@ -1,9 +1,26 @@
+import os
+import re
 import numpy as np
 from PIL import Image
 
 
 def parse_alog_file(alog_path):
-    pass
+    images_info = []
+    coord_n = None
+    coord_e = None
+    with open(alog_path, 'r') as file:
+        lines = file.readlines()
+        for line in lines:
+            if "GPS" in line:
+                # TODO not every line should be parsed, but sometimes there are 2 lines with images in a row so a simople flag won't do the job
+                parts = line.strip().split()[-1].split(',')
+                coord_n = float(parts[0].split('=')[1])
+                coord_e = float(parts[1].split('=')[1])
+            if "LADYBUG_GRAB" in line:
+                parts = line.strip().split()[-1].split(',')
+                image_files = [part.split('=')[1] for part in parts if 'File' in part]
+                images_info.append(((coord_n, coord_e), image_files))
+    return images_info
 
 
 def load_image(image_path):
