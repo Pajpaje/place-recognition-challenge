1094b611795d410038675e1cabc0bc99c05bc380
Pajpaje <114518985+Pajpaje@users.noreply.github.com>
2023-05-12 14:48:30 +0200

    Use learning rate from __init__, not the hardcoded value.

--- model.py
+++ model.py
@@ -47,5 +47,5 @@ class PlaceRecognitionModel(pl.LightningModule):
         return loss
 
     def configure_optimizers(self):
-        optimizer = torch.optim.Adam(self.parameters(), lr=0.001)
+        optimizer = torch.optim.Adam(self.parameters(), lr=self.lr)
         return optimizer
