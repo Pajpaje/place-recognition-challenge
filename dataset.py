Fix parsing of the .alog files. Add ResizeWithPad transform. Add depthwise separable convolution. Add global average pooling so the model can take as input image with arbitrary shape. Fix bugs in dataset class. Find optimal lr automatically.

dataset.py
@@ -1,14 +1,21 @@
 import os
 import glob
+from random import shuffle
 import torch
 from torch.utils.data import Dataset
-from utils import parse_alog_file, load_image, euclidean_distance
+from torchvision.transforms import Compose, ToTensor
+from utils import parse_alog_file, load_image, euclidean_distance, ResizeWithPad
 
 
 class PlaceRecognitionDataset(Dataset):
-    def __init__(self, root_dir, max_distance):
+    def __init__(self, root_dir, max_distance, distance_threshold):
         self.root_dir = root_dir
         self.max_distance = max_distance
+        self.distance_threshold = distance_threshold
+        self.transform = Compose([
+            ToTensor(),
+            ResizeWithPad(size=(256, 256))
+        ])
         self.data = []
 
         images_dir = os.path.join(root_dir, 'images')
@@ -17,32 +24,37 @@ class PlaceRecognitionDataset(Dataset):
         # Get a list of all .alog files recursively
         alog_files = glob.glob(os.path.join(raw_logs_dir, '**/*.alog'), recursive=True)
 
+        gps_and_image_data = []
         for alog_file in alog_files:
-            gps_data, image_data = parse_alog_file(alog_file)
+            gps_and_image_data.extend(parse_alog_file(alog_file))
+        shuffle(gps_and_image_data)
 
-            # Iterate over image_data and create pairs of image sets
-            for i in range(len(image_data)):
-                for j in range(i + 1, len(image_data)):
-                    dist = euclidean_distance(gps_data[i], gps_data[j])
-                    label = 1 if dist <= max_distance else 0
+        # Iterate over image_data and create pairs of image sets
+        for i in range(len(gps_and_image_data)):
+            for j in range(i + 1, len(gps_and_image_data)):
+                dist = euclidean_distance(gps_and_image_data[i][0], gps_and_image_data[j][0])
 
-                    # Get image paths for both sets of images
-                    images_i = [os.path.join(images_dir, img_name) for img_name in image_data[i]]
-                    images_j = [os.path.join(images_dir, img_name) for img_name in image_data[j]]
+                if dist > self.max_distance:
+                    continue
+                # TODO Sometimes we want to compare with places that are further than max_distance
 
-                    self.data.append((images_i, images_j, label))
+                label = 1 if dist <= self.distance_threshold else 0
+
+                # Get image paths for both sets of images
+                images_i = [os.path.join(images_dir, img_name) for img_name in gps_and_image_data[i][1]]
+                images_j = [os.path.join(images_dir, img_name) for img_name in gps_and_image_data[j][1]]
+
+                self.data.append((images_i, images_j, label))
 
     def __len__(self):
         return len(self.data)
 
     def __getitem__(self, idx):
         images_i, images_j, label = self.data[idx]
-
-        # Load and concatenate images
-        images_i = [load_image(img_path) for img_path in images_i]
-        images_j = [load_image(img_path) for img_path in images_j]
-
-        images = torch.cat(images_i + images_j, dim=0)
+        images_i = [self.transform(load_image(img_path)).squeeze(0) for img_path in images_i]
+        images_i = torch.stack(images_i, dim=0)
+        images_j = [self.transform(load_image(img_path)).squeeze(0) for img_path in images_j]
+        images_j = torch.stack(images_j, dim=0)
+        images = torch.cat((images_i, images_j), dim=0)
         label = torch.tensor(label, dtype=torch.float32)
-
         return images, label

model.py
@@ -4,12 +4,27 @@ import torch.nn.functional as F
 import pytorch_lightning as pl
 
 
+class DepthwiseSeparableConv(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, padding):
+        super(DepthwiseSeparableConv, self).__init__()
+        self.depthwise = nn.Conv2d(in_channels, in_channels, kernel_size, padding=padding, groups=in_channels)
+        self.pointwise = nn.Conv2d(in_channels, out_channels, kernel_size=1)
+
+    def forward(self, x):
+        x = self.depthwise(x)
+        x = self.pointwise(x)
+        return x
+
+
 class PlaceRecognitionModel(pl.LightningModule):
-    def __init__(self):
+    def __init__(self, lr=1e-3):
         super(PlaceRecognitionModel, self).__init__()
-        self.conv1 = nn.Conv2d(30, 64, kernel_size=3, padding=1)
-        self.conv2 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
-        self.fc1 = nn.Linear(128 * 96 * 128, 1024)
+        self.lr = lr
+        self.conv1 = nn.Conv2d(10, 32, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
+        self.gap = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc1 = nn.Linear(128, 1024)
         self.fc2 = nn.Linear(1024, 1)
 
     def forward(self, x):
@@ -17,6 +32,8 @@ class PlaceRecognitionModel(pl.LightningModule):
         x = F.max_pool2d(x, 2)
         x = F.relu(self.conv2(x))
         x = F.max_pool2d(x, 2)
+        x = F.relu(self.conv3(x))
+        x = self.gap(x)
         x = x.view(x.size(0), -1)
         x = F.relu(self.fc1(x))
         x = torch.sigmoid(self.fc2(x))

train.py
@@ -1,23 +1,30 @@
-import torch
 from torch.utils.data import DataLoader
 import pytorch_lightning as pl
 from dataset import PlaceRecognitionDataset
 from model import PlaceRecognitionModel
+from pytorch_lightning.tuner.tuning import Tuner
 
 
 def main():
     data_root = 'Eynsham'
-    max_distance = 5.0
-    batch_size = 16
-    num_epochs = 50
+    max_distance = 1000
+    distance_threshold = 200
+    batch_size = 2
+    max_epochs = 2
 
-    dataset = PlaceRecognitionDataset(data_root, max_distance)
+    dataset = PlaceRecognitionDataset(data_root, max_distance, distance_threshold)
+
     dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
-
     model = PlaceRecognitionModel()
+    trainer = pl.Trainer(accelerator="gpu",
+                         max_epochs=max_epochs)
 
-    # Set up the trainer and train the model
-    trainer = pl.Trainer(max_epochs=num_epochs, gpus=int(torch.cuda.is_available()), progress_bar_refresh_rate=20)
+    tun = Tuner(trainer)
+    lr_finder = tun.lr_find(model, dataloader, attr_name="lr")
+    fig = lr_finder.plot(suggest=True)
+    fig.show()
+    model.hparams.lr = lr_finder.suggestion()
+
     trainer.fit(model, dataloader)
 
 

utils.py
@@ -1,31 +1,65 @@
-import os
-import re
+from typing import Tuple, Union
 import numpy as np
+import torch
 from PIL import Image
+from torchvision import transforms
+import torchvision.transforms.functional as F
 
 
 def parse_alog_file(alog_path):
-    images_info = []
+    gps_and_image_info = []
     coord_n = None
     coord_e = None
     with open(alog_path, 'r') as file:
         lines = file.readlines()
         for line in lines:
             if "GPS" in line:
-                # TODO not every line should be parsed, but sometimes there are 2 lines with images in a row so a simople flag won't do the job
                 parts = line.strip().split()[-1].split(',')
-                coord_n = float(parts[0].split('=')[1])
-                coord_e = float(parts[1].split('=')[1])
+                parts_dict = {part.split('=')[0]: part.split('=')[1] for part in parts}
+                coord_n = float(parts_dict.get('N', 0))
+                coord_e = float(parts_dict.get('E', 0))
             if "LADYBUG_GRAB" in line:
                 parts = line.strip().split()[-1].split(',')
-                image_files = [part.split('=')[1] for part in parts if 'File' in part]
-                images_info.append(((coord_n, coord_e), image_files))
-    return images_info
+                image_files = [part.split('=')[1].split('/')[-1] for part in parts if 'File' in part]
+                if coord_n is not None and coord_e is not None:  # only append if we have valid GPS coordinates
+                    gps_and_image_info.append(((coord_n, coord_e), image_files))
+                coord_n = None
+                coord_e = None
+    return gps_and_image_info
 
 
 def load_image(image_path):
-    return np.array(Image.open(image_path))
+    return Image.open(image_path)
 
 
 def euclidean_distance(point1, point2):
     return np.sqrt(np.sum((np.array(point1) - np.array(point2)) ** 2))
+
+
+class ResizeWithPad:
+    """Resizes and pads an image to a target height and width without distortion."""
+    def __init__(self, size: Union[int, Tuple[int, int]]):
+        if type(size) is int:
+            self._target_height = self._target_width = size
+        elif type(size) is tuple:
+            self._target_height, self._target_width = size[0], size[1]
+        else:
+            raise TypeError("Incorrect target shape (should be int or tuple of 2 integers)")
+        self._resize_transform = transforms.Resize(size=(self._target_height, self._target_width))
+
+    def __call__(self, image: torch.Tensor) -> torch.Tensor:
+        scaled_h, scaled_w = self.scaled_image_dims(image)
+        image = transforms.Resize(size=(scaled_h, scaled_w))(image)
+        vp = int((self._target_height - scaled_h) / 2)
+        hp = int((self._target_width - scaled_w) / 2)
+        padding = [hp, vp, hp + (scaled_w % 2) * (hp > 0), vp + (scaled_h % 2) * (vp > 0)]
+        return F.pad(image, padding, 0, "constant")
+
+    def resize_factor(self, image: torch.Tensor) -> int:
+        height_ratio = image.shape[-2] / self._target_height
+        width_ratio = image.shape[-1] / self._target_width
+        return max(max(height_ratio, width_ratio), 1)
+
+    def scaled_image_dims(self, image: torch.Tensor) -> Tuple[int, int]:
+        r_factor = self.resize_factor(image)
+        return int(image.shape[-2] / r_factor), int(image.shape[-1] / r_factor)